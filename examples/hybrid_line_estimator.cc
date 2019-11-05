92fb034733b705a8eef7c2af7261fc5670e3ae8b
Torsten Sattler <sattlert@inf.ethz.ch>
2019-11-05 19:36:35 +0100

    example hybrid solver

---
+++ examples/hybrid_line_estimator.cc
@@ -0,0 +1,155 @@
+// Copyright (c) 2019, Torsten Sattler
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of the copyright holder nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// author: Torsten Sattler, torsten.sattler.de@googlemail.com
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+#include <random>
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+#include <Eigen/Geometry>
+
+#include "hybrid_line_estimator.h"
+
+namespace ransac_lib {
+
+HybridLineEstimator::HybridLineEstimator(
+    const Eigen::Matrix2Xd& points,
+    const Eigen::Matrix4Xd& points_with_normals) {
+  points_ = points;
+  num_points_ = points_.cols();
+  points_with_normals_ = points_with_normals;
+  num_points_with_normals_ = points_with_normals_.cols();
+}
+
+int LineEstimator::LeastSquares(const std::vector<std::vector<int>>& sample,
+                                Eigen::Vector3d* line) const {
+  const int kNumSamplesPoints = static_cast<int>(sample[0].size());
+  const int kNumSamplesPointsWithNormals = static_cast<int>(sample[1].size());
+  const int kNumSamples = kNumSamplesPoints + kNumSamplesPointsWithNormals;
+  
+  if (kNumSamples < 6) return 0;
+  // We fit the line by estimating the eigenvectors of the covariance matrix
+  // of the data.
+  Eigen::Vector2d mean(0.0, 0.0);
+  for (int i = 0; i < kNumSamplesPoints; ++i) {
+    mean += points_.col(sample[0][i]);
+  }
+  for (int i = 0; i < kNumSamplesPointsWithNormals; ++i) {
+    mean += points_with_normals_.col(sample[1][i]).head<2>();
+  }
+  mean /= static_cast<double>(kNumSamples);
+
+  // Builds the covariance matrix C.
+  Eigen::Matrix2d C = Eigen::Matrix2d::Zero();
+
+  for (int i = 0; i < kNumSamplesPoints; ++i) {
+    Eigen::Vector2d d = points_.col(sample[0][i]) - mean;
+    C += d * d.transpose();
+  }
+  for (int i = 0; i < kNumSamplesPointsWithNormals; ++i) {
+    Eigen::Vector2d d = points_with_normals_.col(sample[1][i]) - mean;
+    C += d * d.transpose();
+  }
+  C /= static_cast<double>(kNumSamples - 1);
+
+  Eigen::SelfAdjointEigenSolver<Eigen::Matrix2d> eig_solver(C);
+  if (eig_solver.info() != Eigen::Success) return 0;
+
+  line->head<2>() = eig_solver.eigenvectors().col(1);
+
+  // Re-estimates the translation along the line to account for subtraction
+  // of mean.
+  (*line)[2] = -line->head<2>().dot(mean);
+
+  return 1;
+}
+
+// Evaluates the line on the i-th data point of the t-th data type.
+double LineEstimator::EvaluateModelOnPoint(const Eigen::Vector3d& line, int t,
+                                           int i) const {
+  double residual = 0.0;
+  if (t == 0) {
+    residual = line.dot(points_.col(i).homogeneous());
+  } else {
+    residual = line.dot(points_with_normals.col(i).head<2>().homogeneous());
+  }
+  return residual * residual;
+}
+
+int LineEstimator::TwoPointSolver(const std::vector<int>& sample,
+                                  std::vector<Eigen::Vector3d>* lines) const {
+  lines->clear();
+  if (sample.size() < 2u) return 0;
+
+  lines->resize(1);
+  Eigen::Vector3d p1(points_(0, sample[0]), points_(1, sample[0]), 1.0);
+  Eigen::Vector3d p2(points_(0, sample[1]), points_(1, sample[1]), 1.0);
+  (*lines)[0] = p1.cross(p2);
+  // Normalizes the line such that the normal of the line has unit length.
+  double normal_norm = (*lines)[0].head<2>().norm();
+  if (normal_norm == 0.0) {
+    lines->clear();
+    return 0;
+  }
+
+  (*lines)[0] /= normal_norm;
+
+  return 1;
+}
+
+int LineEstimator::PointNormalSolver(
+    const std::vector<int>& sample, std::vector<Eigen::Vector3d>* lines) const {
+  lines->clear();
+  if (sample.size() < 1u) return 0;
+
+  lines->resize(1);
+  Eigen::Vector2d normal = points_with_normals_.col(sample[0]).tail<2>();
+  normal.normalize();
+  double c = -normal.dot(points_with_normals_.col(sample[0]).head<2>());
+  (*lines)[0] = Eigen::Vector3d(normal[0], normal[1], c);
+  // Normalizes the line such that the normal of the line has unit length.
+  double normal_norm = (*lines)[0].head<2>().norm();
+  if (normal_norm == 0.0) {
+    lines->clear();
+    return 0;
+  }
+
+  (*lines)[0] /= normal_norm;
+
+  return 1;
+}
+
+}  // namespace ransac_lib
---
+++ examples/hybrid_line_estimator.h
@@ -0,0 +1,112 @@
+// Copyright (c) 2019, Torsten Sattler
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Torsten Sattler nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// author: Torsten Sattler, torsten.sattler.de@googlemail.com
+
+#ifndef RANSACLIB_EXAMPLE_LINE_ESTIMATOR_H_
+#define RANSACLIB_EXAMPLE_LINE_ESTIMATOR_H_
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+#include <random>
+#include <vector>
+
+#include <Eigen/Core>
+#include <Eigen/StdVector>
+
+namespace ransac_lib {
+
+// Implements hybrid solver that estimates a line from either two data points
+// or from a point with a known normal.
+// Solver 1 takes two points as input, solver 2 one point with a normal.
+// The first data type is 2D points, the second points with normals.
+class HybridLineEstimator {
+ public:
+  HybridLineEstimator(const Eigen::Matrix2Xd& points,
+                      const Eigen::Matrix4Xd& points_with_normals);
+
+  inline int num_minimal_solvers() const { return 2; }
+
+  inline void min_sample_sizes(
+      std::vector<std::vector<int>>* min_sample_sizes) const {
+    min_sample_sizes->resize(2);
+    (*min_sample_sizes)[0] = {2, 0};
+    (*min_sample_sizes)[1] = {0, 1};
+  }
+
+  inline int num_data_types() const { return 2; }
+
+  inline void num_data(std::vector<int>* num_data) const {
+    num_data->resize(2);
+    (*num_data)[0] = num_points_;
+    (*num_data)[1] = num_points_with_normals_;
+  }
+
+  // Uniform prior probabilities.
+  inline void solver_probabilities(
+      std::vector<double>* solver_probabilites) const {
+    solver_probabilites->resize(2);
+    (*solver_probabilites)[0] = 0.5;
+    (*solver_probabilites)[1] = 0.5;
+  }
+
+  inline int MinimalSolver(const std::vector<std::vector<int>>& sample,
+                           const int solver_idx,
+                           std::vector<Eigen::Vector3d>* lines) const {
+    if (solver_idx == 0) {
+      return TwoPointSolver(sample[0], lines);
+    } else {
+      return PointNormalSolver(sample[1], lines);
+    }
+  }
+
+  // Evaluates the line on the i-th data point of the t-th data type.
+  double EvaluateModelOnPoint(const Eigen::Vector3d& line, int t, int i) const;
+
+  // Linear least squares solver. Calls NonMinimalSolver.
+  void LeastSquares(const std::vector<std::vector<int>>& sample,
+                    Eigen::Vector3d* line) const;
+
+ protected:
+  int TwoPointSolver(const std::vector<int>& sample,
+                     std::vector<Eigen::Vector3d>* lines) const;
+  int PointNormalSolver(const std::vector<int>& sample,
+                        std::vector<Eigen::Vector3d>* lines) const;
+
+  Eigen::Matrix2Xd points_;
+  int num_points_;
+  Eigen::Matrix4Xd& points_with_normals_;
+  int num_points_with_normals_;
+};
+
+}  // namespace ransac_lib
+
+#endif  // RANSACLIB_EXAMPLE_LINE_ESTIMATOR_H_
